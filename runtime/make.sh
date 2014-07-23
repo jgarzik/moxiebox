aab36ae00d6ea81c191c4ac29e8d815e87a3a67e
Jeff Garzik <jgarzik@bitpay.com>
2014-07-23 13:36:24 -0400

    runtime: add mmap

--- runtime/make.sh
+++ runtime/make.sh
@@ -2,4 +2,9 @@
 
 moxie-elf-as -EL -o crt0.o crt0.S
 moxie-elf-as -EL -o sys-exit.o sys-exit.S
-moxie-elf-ar rvs --target=elf32-littlemoxie libsandboxrt.a crt0.o sys-exit.o
+moxie-elf-as -EL -o sys-mmap.o sys-mmap.S
+moxie-elf-ar rvs --target=elf32-littlemoxie libsandboxrt.a \
+	crt0.o \
+	sys-exit.o \
+	sys-mmap.o
+
