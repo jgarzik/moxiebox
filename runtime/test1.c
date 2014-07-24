e68d8b9d35cf0792f8a4081635fd82cc2a3f6899
Jeff Garzik <jgarzik@bitpay.com>
2014-07-24 00:03:38 -0400

    runtime: make test1 more complex (triggering creation of .data, .bss)

--- runtime/test1.c
+++ runtime/test1.c
@@ -2,9 +2,12 @@
 #include <stddef.h>
 #include "sandboxrt.h"
 
+int prot = 0x11;
+int flags = 0;
+
 int main (int argc, char *argv[])
 {
-	void *p = mmap(NULL, 0, 0, 0, 0, 0);
+	void *p = mmap(NULL, 0, prot, flags, 0, 0);
 	_exit(0);
 	return 0;
 
